e970d76a25deca29bb373c89949fa2f406cee25c
GamelabAhmad <164120476+GamelabAhmad@users.noreply.github.com>
2024-06-14 20:54:17 +0700

    Update connection.js
    config db

--- backend/model/connection.js
+++ backend/model/connection.js
@@ -2,11 +2,11 @@ const mysql = require('mysql');
 require('dotenv').config();
 
 const db = mysql.createConnection({
-    host: process.env.host,
-    user: process.env.user,
-    password: process.env.password,
-    database: process.env.database,
-    port: 3307 
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.SB_PASS,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
 });
 
 db.connect((error) => {
